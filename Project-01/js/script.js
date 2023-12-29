c9e0265f5a0b85760a858ab48e0c2df698e91c48
Bestimran <imrantahir9918@gmail.com>
2023-12-29 15:08:20 +0600

    html and css done

--- Project-01/js/script.js
+++ Project-01/js/script.js
@@ -1,1 +1,115 @@
-console.log("you will be able to do it keep practicing");
+let Products = {
+  data: [
+    {
+      ProductName: "best hoodie",
+      category: "Top wear",
+      price: "100",
+      image: "../images/images (1).jpg",
+    },
+    {
+      ProductName: "best hoodie",
+      category: "Top wear",
+      price: "100",
+      image: "../images/images (2).jpg",
+    },
+    {
+      ProductName: "best hoodie",
+      category: "Top wear",
+      price: "100",
+      image: "../images/images.jpg",
+    },
+    {
+      ProductName: "best hoodie",
+      category: "Top wear",
+      price: "100",
+      image: "../images/download (1).jpg",
+    },
+    {
+      ProductName: "shirt best for man",
+      category: "super",
+      price: "200",
+      image: "../images/images (3).jpg",
+    },
+    {
+      ProductName: "shirt best for man",
+      category: "super",
+      price: "200",
+      image: "../images/shirt.jpg",
+    },
+    {
+      ProductName: "shirt best for man",
+      category: "super",
+      price: "200",
+      image: "../images/fond.jpg",
+    },
+    {
+      ProductName: "watch collection",
+      category: "special",
+      price: "500",
+      image: "../images/watch (2).jpg",
+    },
+    {
+      ProductName: "watch collection",
+      category: "special",
+      price: "500",
+      image: "../images/w4).jpg",
+    },
+    {
+      ProductName: "watch collection",
+      category: "special",
+      price: "500",
+      image: "../images/w).jpg",
+    },
+
+    {
+      ProductName: "pent collection",
+      category: "special",
+      price: "600",
+      image: "../images/6873cc736c4e0b2afd5f779e4c92d491.jpg",
+    },
+    {
+      ProductName: "pent collection",
+      category: "special",
+      price: "600",
+      image: "../images/713wqBoyWpL._UX679_.jpg",
+    },
+    {
+      ProductName: "pent collection",
+      category: "special",
+      price: "600",
+      image: "../images/ga8ew_512.jpg",
+    },
+    {
+      ProductName: "pent collection",
+      category: "special",
+      price: "600",
+      image: "../images/Mens-Blue-Track-Pent.jpg",
+    },
+    {
+      ProductName: "shoes collection",
+      category: "best",
+      price: "1000",
+      image: "../images/shoes).jpg",
+    },
+    {
+      ProductName: "shoes collection",
+      category: "best",
+      price: "1000",
+      image: "../images/shoes(2).jpg",
+    },
+    {
+      ProductName: "shoes collection",
+      category: "best",
+      price: "1000",
+      image: "../images/p2153171.avif",
+    },
+    {
+      ProductName: "shoes collection",
+      category: "best",
+      price: "1000",
+      image: "../images/ROLLZ_22G-862_WHT-V.ORG_2.webp",
+    },
+  ],
+};
+
+console.log(Products.data.length);
